2344c19b3d805386c1cb8492683a615f4776a801
Jami <jamischwarzwalder@gmail.com>
2016-09-23 09:38:16 -0700

    Building Playing Card class

---
+++ Java Review/src/edu/greenriver/jschwarzwalder/Blackjack/PlayingCards.java
@@ -0,0 +1,37 @@
+package edu.greenriver.jschwarzwalder.Blackjack;
+
+public class PlayingCards {
+	private String color;
+	private int width;
+	private int height;
+	
+	public PlayingCards(String mColor, int mWidth, int mHeight ) {
+		color = mColor;
+		width = mWidth;
+		height = mHeight;
+		
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	
+
+
+}
--- Java Review/src/edu/greenriver/jschwarzwalder/review/Console.java
+++ Java Review/src/edu/greenriver/jschwarzwalder/review/Console.java
@@ -3,113 +3,76 @@ package edu.greenriver.it.console;
 import java.util.Scanner;
 
 /**
- * Helper class for reading and writing to the Java console.
- * 
- * @author Josh Archer
- * @version 1.0
+ * Helper class for reading and writing to the Java console. * * @author Josh
+ * Archer * @version 1.0
  */
-public class Console
-{
+public class Console {
 	private static Scanner console = new Scanner(System.in);
-	
+
 	/**
-	 * Prints a message to the Java console.
-	 * 
-	 * @param message the message to be printed
+	 * * Prints a message to the Java console. * * @param message the message to
+	 * be printed
 	 */
-	public static void print(String message)
-	{
+	public static void print(String message) {
 		System.out.println(message);
 	}
-	
+
 	/**
-	 * Retrieves a String value from the Java console.
-	 * 
-	 * @param prompt a prompt for the user to read before
-	 * entering input
-	 * @return a String value
+	 * * Retrieves a String value from the Java console. * * @param prompt a
+	 * prompt for the user to read before * entering input * @return a String
+	 * value
 	 */
-	public static String getString(String prompt)
-	{
+	public static String getString(String prompt) {
 		System.out.println(prompt + ": ");
 		return console.nextLine();
 	}
-	
+
 	/**
-	 * Retrieves an int value from the Java console.
-	 * 
-	 * @param prompt a prompt for the user to read before
-	 * entering input
-	 * @return an int value
+	 * * Retrieves an int value from the Java console. * * @param prompt a
+	 * prompt for the user to read before * entering input * @return an int
+	 * value
 	 */
-	public static int getInt(String prompt)
-	{
+	public static int getInt(String prompt) {
 		System.out.println(prompt + ": ");
-		while (!console.hasNextInt())
-		{
+		while (!console.hasNextInt()) {
 			System.out.println("Please enter a valid integer: ");
-			
-			//clear the scanner buffer
-			console.nextLine();
+			// clear the scanner buffer console.nextLine();
 		}
-		
-		int result = console.nextInt();
-		
-		//clear the scanner buffer
+		int result = console.nextInt(); // clear the scanner buffer
 		console.nextLine();
-		
 		return result;
 	}
 
 	/**
-	 * Retrieves a double value from the Java console.
 	 * 
-	 * @param prompt a prompt for the user to read before
-	 * entering input
-	 * @return a double value
+	 * Retrieves a double value from the Java console. * * @param prompt a
+	 * prompt for the user to read before * entering input * @return a double
+	 * value
 	 */
-	public static double getDouble(String prompt)
-	{
+	public static double getDouble(String prompt) {
 		System.out.println(prompt + ": ");
-		while (!console.hasNextDouble())
-		{
+		while (!console.hasNextDouble()) {
 			System.out.println("Please enter a valid double: ");
-			
-			//clear the scanner buffer
-			console.nextLine();
+			// clear the scanner buffer console.nextLine();
 		}
-		
-		double result = console.nextDouble();
-		
-		//clear the scanner buffer
+		double result = console.nextDouble(); // clear the scanner buffer
 		console.nextLine();
-		
 		return result;
 	}
 
 	/**
-	 * Retrieves a boolean value from the Java console.
-	 * 
-	 * @param prompt a prompt for the user to read before
-	 * entering input
-	 * @return a boolean value
+	 * * Retrieves a boolean value from the Java console. * * @param prompt a
+	 * prompt for the user to read before * entering input * @return a boolean
+	 * value
 	 */
-	public static boolean getBoolean(String prompt)
-	{
+	public static boolean getBoolean(String prompt) {
 		System.out.println(prompt + ": ");
-		while (!console.hasNextBoolean())
-		{
+		while (!console.hasNextBoolean()) {
 			System.out.println("Please enter true/false: ");
-			
-			//clear the scanner buffer
-			console.nextLine();
+			// clear the scanner buffer console.nextLine();
 		}
-		
-		boolean result = console.nextBoolean();
-		
-		//clear the scanner buffer
+		boolean result = console.nextBoolean(); // clear the scanner buffer
 		console.nextLine();
-		
 		return result;
 	}
 }
