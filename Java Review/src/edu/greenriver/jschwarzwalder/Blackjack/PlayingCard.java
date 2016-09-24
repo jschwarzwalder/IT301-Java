ebd30e9c7c1a71bd4806599aafc2d33de62f515a
Jami <jamischwarzwalder@gmail.com>
2016-09-24 14:32:02 -0700

    Adding Java Docs and more logic for shuffle

--- Java Review/src/edu/greenriver/jschwarzwalder/Blackjack/PlayingCard.java
+++ Java Review/src/edu/greenriver/jschwarzwalder/Blackjack/PlayingCard.java
@@ -27,24 +27,65 @@ public class PlayingCard {
 		
 	}
 	
+	/**
+	 * Returns the int value of the current color of the card
+	 * 
+	 * @return int The current color of card
+	 */
 	public String getColor() {
 		return color;
 	}
+	
+	/**
+	 * This will change the string value for the color of the card
+	 * 
+	 * @param color a string that represents color of card
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
+	
+	/**
+	 * Returns the int value of the current width of the card
+	 * 
+	 * @return int The current width of card
+	 */
 	public int getWidth() {
 		return width;
 	}
+	
+	/**
+	 * This will change the int value for the width of the card
+	 * 
+	 * @param width a number that represents width of card
+	 */
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	
+	/**
+	 * Returns the int value of the current height of the card
+	 * 
+	 * @return int The current height of card
+	 */
 	public int getHeight() {
 		return height;
 	}
+	
+	/**
+	 * This will change the int value for the height of the card
+	 * 
+	 * @param height a number that represents height of card
+	 */
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	
+	/**
+	 * Creates human readable description of card including color, height and width
+	 * 
+	 * @return String that identifies color, height, and width of card
+	 */
 	
 	public String toString(){
 		String cardString ="This card is " + color + " , " + height + " tall, and " + width + " wide.";
