3176a3f50dceba7e13072d0ca0c7a457b9e0a569
Jami <jamischwarzwalder@gmail.com>
2016-10-03 08:27:42 -0700

    print out each digit in binary number

--- Bitwise Operators/src/edu/greenriver/it/subsets/CountingInBinary.java
+++ Bitwise Operators/src/edu/greenriver/it/subsets/CountingInBinary.java
@@ -10,15 +10,21 @@ public class CountingInBinary {
 	}
 	
 	public static void printBinary(int number, int howManyBits) {
+		//for loop 
+		for(int i = howManyBits - 1 ; i >= 0; i--){
+			int mask = (int) Math.pow(2, i);
+			int result = number & mask;
+			
+			if (result == 0){ //the bit is "off" 
+				System.out.print("0");
+			} else { //the bit is "on"
+				System.out.print("1");
+			}
+		}
+		
 		//print number in binary
 		int mask = 1;
-		int result = number & mask;
 		
-		if (result == 0){ //the bit is "off" 
-			System.out.print("0");
-		} else { //the bit is "on"
-			System.out.print("1");
-		}
 		
 		//print new line
 		System.out.println();
