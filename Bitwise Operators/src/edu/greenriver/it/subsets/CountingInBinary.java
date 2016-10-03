f14f5bb25a65ba0adfe69762be28de34a21492c2
Jami <jamischwarzwalder@gmail.com>
2016-10-03 08:45:49 -0700

    calculating to 8 bits

--- Bitwise Operators/src/edu/greenriver/it/subsets/CountingInBinary.java
+++ Bitwise Operators/src/edu/greenriver/it/subsets/CountingInBinary.java
@@ -4,28 +4,29 @@ public class CountingInBinary {
 	
 	public static void main(String[] args){
 		//print out 0-7 in binary
-		for (int i = 0; i < 8; i++){
-			printBinary(i,3);
+		for (int i = 0; i < Math.pow(2,  8); i++){
+			printBinary(i,8);
 		}
 	}
 	
 	public static void printBinary(int number, int howManyBits) {
+		
 		//for loop 
-		String binary = "";
-		for(int i = 0; i < howManyBits; i++){
+		for(int i = howManyBits - 1 ; i >= 0; i--){
 			int mask = (int) Math.pow(2, i);
 			int result = number & mask;
 			
 			if (result == 0){ //the bit is "off" 
-				//System.out.print("0");
-				binary = "0" + binary;
+				System.out.print("0");
 			} else { //the bit is "on"
-				binary = "1" + binary;
+				System.out.print("1");
 			}
 		}
 		
 		
+		
+		
 		//print new line
-		System.out.println(binary);
+		System.out.println();
 	}
 }
