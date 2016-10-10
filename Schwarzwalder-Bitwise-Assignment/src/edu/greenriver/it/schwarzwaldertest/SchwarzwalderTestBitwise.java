90323e8491c702b9df25b86eadc5c972f80b47e9
Jami <jamischwarzwalder@gmail.com>
2016-10-09 21:39:39 -0700

    Adding Comment to Bitwise test

--- Schwarzwalder-Bitwise-Assignment/src/edu/greenriver/it/schwarzwaldertest/SchwarzwalderTestBitwise.java
+++ Schwarzwalder-Bitwise-Assignment/src/edu/greenriver/it/schwarzwaldertest/SchwarzwalderTestBitwise.java
@@ -1,3 +1,10 @@
+/*
+ * Jami Schwarzwalder
+ * 10/8/2016
+ * SchwarzwalderTestBitwise.java
+ * This file tests Employee and Subsets Classes!
+ */
+
 package edu.greenriver.it.schwarzwaldertest;
 
 import edu.greenriver.it.subsets.Subsets;
