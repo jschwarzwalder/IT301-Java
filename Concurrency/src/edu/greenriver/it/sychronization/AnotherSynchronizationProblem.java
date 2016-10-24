ab5a2224c4bf7f5ff6a8bc39e50e18245a107049
Jami <jamischwarzwalder@gmail.com>
2016-10-24 09:25:37 -0700

    Using System.out to lock thread

--- Concurrency/src/edu/greenriver/it/sychronization/AnotherSynchronizationProblem.java
+++ Concurrency/src/edu/greenriver/it/sychronization/AnotherSynchronizationProblem.java
@@ -6,16 +6,19 @@ public class AnotherSynchronizationProblem {
 		//start a few threads
 		getPrintingThread(new String[] {"fifth element", "gladior", 
 				"kiki's delivery service", "star wars", "ever after", "sharknado"}).start();
+		getPrintingThread(new String[] {"star trek", "flash", 
+				"stargate", "walking dead", "orphan black", "Doctor Who"}).start();
 	}
 	
 	public static Thread getPrintingThread(String[] things){
 		return new Thread(new Runnable(){
 			public void run(){
 				for (int i = 0; i< things.length; i++){
-					System.out.println("<<");
-					System.out.println(things[i]);
-					System.out.println(">>");
-				
+					synchronized (System.out){
+						System.out.print("<<");
+						System.out.print(things[i]);
+						System.out.println(">>");
+					}
 				}
 			}
 		});
